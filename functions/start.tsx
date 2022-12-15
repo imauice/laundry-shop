b69ae83dd39ac5a4f0b877c97b2172816952af2b
imauice <102501547+imauice@users.noreply.github.com>
2022-12-15 16:00:23 +0700

    modified:   functions/start.tsx 	new file:   vercel.json

--- functions/start.tsx
+++ functions/start.tsx
@@ -1,15 +1,15 @@
-
-export default function Start(id:string|undefined){
+export default async function Start(id:string|undefined){
 
 
        const machine_id = id;
     var requestOptions: RequestInit = {
       method: 'POST',
-      headers: { "secret_key": `${process.env.NEXT_SECRET_KEY}` },
+      headers: {"secret_key": `${process.env.NEXT_SECRET_KEY}`},
       redirect: 'follow'
     };
-    const url = `/api/machinestart?machine_id=${machine_id}`
-    fetch(url, requestOptions).then((res)=>{
+   
+    const url = `https://laundry-shop-nine.vercel.app/api/machinestart?machine_id=${machine_id}`
+   await fetch(url, requestOptions).then((res)=>{
       if(res.statusText == 'OK'){
           console.log(`${machine_id} is started`);
           location.reload();
